6a58c2ba79755905c63850fc94ac3e76c8925a60
DESKTOP-V08TQ4V\Domagoj <melija92@gmail.com>
2018-03-10 19:38:36 +0100

    Kreiran početni WebApi projekt

---
+++ DsucicOmegaEdukacija/DsucicOmegaEdukacija/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DsucicOmegaEdukacija.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ DsucicOmegaEdukacija/DsucicOmegaEdukacija/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DsucicOmegaEdukacija.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
